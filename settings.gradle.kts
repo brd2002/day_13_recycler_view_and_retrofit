eac62d27912c3688a30bdef9d4f91adc8af28981
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-03 12:54:27 +0530

    this is the first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Day_13_wallpaper_app"
+include(":app")
+ 
